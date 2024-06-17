55a8b222dd88acf5b2d023955a855d0c5e4d09d4
苏鹏川 <supengchuan@datacloak.com>
2024-06-17 15:06:38 +0800

    add pwsh config

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,47 @@
+#-------------------------------  Set Hot-keys BEGIN  -------------------------------
+Set-PSReadLineOption -EditMode Emacs
+# 设置预测文本来源为历史记录
+Set-PSReadLineOption -PredictionSource History
+# 每次回溯输入历史，光标定位于输入内容末尾
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+
+# 设置 Tab 为菜单补全和 Intellisense
+Set-PSReadLineKeyHandler -Key "Tab" -Function MenuComplete
+
+# 设置 Ctrl+d 为退出 PowerShell
+Set-PSReadlineKeyHandler -Key "Ctrl+d" -Function ViExit
+
+# 设置 Ctrl+z 为撤销
+Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo
+
+# 设置向上键为后向搜索历史记录
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+
+# 设置向下键为前向搜索历史纪录
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+#-------------------------------  Set Hot-keys END    -------------------------------
+
+
+#-------------------------------   Set Alias BEGIN    -------------------------------
+function ListDirectory {
+	(Get-ChildItem).Name
+	Write-Host("")
+}
+Set-Alias -Name ls -Value ListDirectory
+Set-Alias -Name ll -Value Get-ChildItem
+
+# 打开当前工作目录
+function OpenCurrentFolder {
+	param
+	(
+		# 输入要打开的路径
+		# 用法示例：open C:\
+		# 默认路径：当前工作文件夹
+		$Path = '.'
+	)
+	Invoke-Item $Path
+}
+Set-Alias -Name open -Value OpenCurrentFolder
+Set-Alias -Name lz -Value  lazygit.exe
+
+#-------------------------------    Set Alias END     -------------------------------
