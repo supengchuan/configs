66e2679ee9b9c1edd2acfe6bdf7ee836ed205488
苏鹏川 <supengchuan@datacloak.com>
2024-06-21 15:55:01 +0800

    fix polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/bash
 
 # Terminate already running bar instances
-# pkill -9 polybar
-ps -ef | grep polybar | grep -v grep | grep -v launch | awk '{print $2}'| xargs kill -9
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# If all your bars have ipc enabled, you can use
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
+# or
+# ps -ef | grep polybar | grep -v grep | grep -v launch | awk '{print $2}'| xargs kill -9
 
 # Launch polybar
-polybar bar -c ~/.config/polybar/config.ini >/dev/null 2>&1 &
-sleep 1s
-polybar left -c ~/.config/polybar/config.ini >/dev/null 2>&1 &
-#sleep 1s
-polybar right -c ~/.config/polybar/config.ini >/dev/null 2>&1 &
+# Launch bar and left bar, right bar
+echo "---" | tee -a /tmp/bar.log /tmp/left-bar.log /tmp/right-bar.log
+polybar bar 2>&1 | tee -a /tmp/bar.log &
+disown
+polybar left 2>&1 | tee -a /tmp/left-bar.log &
+disown
+polybar right 2>&1 | tee -a /tmp/right-bar.log &
+disown
 
-
+echo "Bars launched..."
