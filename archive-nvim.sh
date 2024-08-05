0e92dccf7d420474b52964aa1b8706583d106760
苏鹏川 <supengchuan@datacloak.com>
2024-08-05 17:23:09 +0800

    add change mason link script

--- archive-nvim.sh
+++ archive-nvim.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 if [ -d ./out ]; then
 	rm -rf out
 fi
@@ -31,6 +29,38 @@ fi
 
 EOF
 
-chmod +x out/move.sh
+cat << "EOF" > out/update_links.sh
+#!/bin/bash
+
+# Define the old and new user
+OLD_USER=""
+NEW_USER=""
+
+# Loop through all symbolic links in the current directory
+for link in *; do
+	# Check if it is a symbolic link
+	if [ -L "$link" ]; then
+		# Get the current target of the symbolic link
+		current_target=$(readlink "$link")
+
+		echo "$current_target"
+		# Check if the target contains the old directory path
+		if [[ "$current_target" == "/home/$OLD_USER"* ]]; then
+			# Replace the old directory path with the new directory path
+			new_target=${current_target/$OLD_USER/$NEW_USER}
+
+			# Remove the old symbolic link
+			rm "$link"
+
+			# Create a new symbolic link with the updated target
+			ln -s "$new_target" "$link"
+
+			echo "Updated $link -> $new_target"
+		fi
+	fi
+done
+EOF
+
+chmod +x out/move.sh out/update_links.sh
 tar -zcvf out.tar.gz out
 rm -rf out
